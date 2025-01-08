f6477d8fb3009026b3e14ed03c3b7302f6da910f
idontwanttobehereanymore <daniel.rimpl@schueler.6072-fuerth.de>
2025-01-08 15:36:31 +0100

    nice changes

--- htdocs/hundred_numbers.php
+++ htdocs/hundred_numbers.php
@@ -1,16 +1,37 @@
 <?php
-$size = 10;
-$count = 0;
+$row_size = 10;
+$col_size = 10;
 echo "<table>";
 echo "<tr>";
-for ($i=1; $i<=$size; $i++ ){
-    echo "<td style='border-style:solid; width: 40px; height: 40px'>$count</td>";
-    $count += 1;
-    if (($i % $size) == 0 && $count < $size*$size){
+
+function get_prime_mapping_from_range($max) {
+    $prime_map = array_fill(0, $max + 1, true);
+    $prime_map[0] = $prime_map[1] = false;
+    // Sieve of Eratosthenes
+    for ($i = 2; $i*$i <= $max; $i++) {
+        if ($prime_map[$i]) {
+            for ($j = $i*$i; $j <= $max; $j += $i) {
+                $prime_map[$j] = false;
+            }
+        }
+    }
+    return $prime_map;
+}
+
+$prime_mapping = get_prime_mapping_from_range($row_size * $col_size);
+$count = 1;
+for ($i=1; $i<=$row_size; $i++ ){
+    $color = "white";
+    if ($prime_mapping[$count]){
+        $color = "yellow";
+    }
+    echo "<td style='border-style:solid; background-color:$color; width: 40px; height: 40px'>$count</td>";
+    if (($i % $row_size) == 0 && $count < $row_size*$col_size){
         echo "</tr>";
         $i = 0;
         echo "<tr>";
     }
+    $count += 1;
 }
 echo "<tr>";
 echo "</table>";
